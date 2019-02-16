fe59fb3efc230e1150fdd64b459f6237efa4972b
Shajupaul Paulson <shaju.eee@gmail.com>
2019-02-16 12:23:21 +0100

    Server client message queue and signal handling complete

--- Signalshandling/client/SingalreceiverClinet.cpp
+++ Signalshandling/client/SingalreceiverClinet.cpp
@@ -7,8 +7,13 @@
 #include <boost/interprocess/sync/interprocess_mutex.hpp>
 #include <sys/ipc.h> 
 #include <sys/msg.h> 
+typedef struct mymsg {
+  long mtype;
+  int temp_pid;
+ }mymsg;
 
 static int s_pid=-1;
+static int t_pid = -1;
 static bool ServerLost = true;
 void serverAlive(){
 	while (!SignalReceiver::getStopInfo()){
@@ -20,51 +25,73 @@ void serverAlive(){
 		else {
 			//Server Process lost Ser s_pid to -1 and 
 			s_pid = -1;
+			ServerLost = true;
 
 		}
 		sleep(1); //Sleep 1 sec for next alive check
 	}
 }
+void Messaging(){
+	key_t key,key2; 
+	int msgid,msgid2; 
+	mymsg msg,buff;  
+	// ftok for client queue 
+	key = ftok("Client", 20); 
+	msgid = msgget(key, 0666 | IPC_CREAT); 
 
+	//ftok for server queue
+	key2 = ftok("Server", 20); 
+	msgid2 = msgget(key2, 0666 | IPC_CREAT); 
+	while (!SignalReceiver::getStopInfo()){
+	   if(ServerLost){
+		cout << " About to receive" << endl;
+		msgrcv(msgid2, &buff, sizeof(mymsg), 1, 0);
+
+		s_pid = buff.temp_pid;	
+		cout <<" Received " << s_pid <<endl;
+		msg.temp_pid=t_pid;
+
+		msg.mtype = 1;
+		msgsnd(msgid, &msg, sizeof(msg), 0);
+		cout <<" Send " <<endl;
+		ServerLost = false;
+		
+	  }
+   	  sleep (1);
+	}
+	msgctl(msgid, IPC_RMID, NULL);
+	msgctl(msgid2, IPC_RMID, NULL);
+}
 int main() {
 
 SignalReceiver m_signalReceiver;
 m_signalReceiver.SetSignalHandler();
-int t_pid = getpid ();
-key_t key,key2; 
-int msgid,msgid2; 
-  
-// ftok for client queue 
-key = ftok("Client", 20); 
-msgid = msgget(key, 0666 | IPC_CREAT); 
+t_pid = getpid ();
 
-//ftok for server queue
-key2 = ftok("Server", 20); 
-msgid2 = msgget(key2, 0666 | IPC_CREAT); 
 
 
 boost::interprocess::interprocess_mutex mut;
-mut.lock();
+//mut.lock();
 thread t1(serverAlive);
+thread t2(Messaging);
 	while (!SignalReceiver::getStopInfo()){
 		if (s_pid != -1){
 
 		}
 		else{
 			//set a condition wait for server to come up
-			mut.lock();
+			//mut.lock();
 			// msgsnd to send message 
-			msgsnd(msgid, &t_pid, sizeof(t_pid), 0);
-			int ls_pid;
-			msgrcv(msgid, &ls_pid, sizeof(ls_pid), 1, 0);
-			s_pid = ls_pid;
+
+
 		}
+
 	}
 
 
 
 t1.join();
-
+t2.join();
 
 return 0;
 
--- Signalshandling/server/signalSender.cpp
+++ Signalshandling/server/signalSender.cpp
@@ -1,8 +1,92 @@
-#include <mqueue.h>
 #include <iostream>
+#include <mqueue.h>
+#include <thread>
+#include  <sys/types.h>
+#include  <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <boost/interprocess/sync/interprocess_mutex.hpp>
+#include <sys/ipc.h> 
+#include <sys/msg.h> 
+
+using namespace std;
+typedef struct mymsg {
+  long mtype;
+  int temp_pid;
+ }mymsg;
 
 int main (){
+int t_pid = getpid ();
+key_t key,key2; 
+int msgid,msgid2; 
+mymsg msg,buff;  
+// ftok for client queue 
+key = ftok("Client", 20); 
+msgid = msgget(key, 0666 | IPC_CREAT); 
 
+//ftok for server queue
+key2 = ftok("Server", 20); 
+msgid2 = msgget(key2, 0666 | IPC_CREAT); 
+boost::interprocess::interprocess_mutex mut;
+//mut.unlock();
+int c_pid=-1;
+msg.mtype=1;
+msg.temp_pid=t_pid;
+msgsnd(msgid2, &msg, sizeof(mymsg), 0);
+cout <<" Send " <<endl;
 
-	return 0;
+msgrcv(msgid, &buff, sizeof(mymsg), 1, 0);
+c_pid=buff.temp_pid;
+cout <<" Received " << c_pid <<endl;
+
+cout << "Provide the signal you want to send or exit" << endl;
+bool exitloop=false;
+int input;
+while (!exitloop){
+cout << "Signal list: \n1.SIGABRT \n2.SIGFPE \n3.SIGILL \n4.SIGINT \n5.SIGSEGV \n6.SIGTERM \
+	\n7.SIGTSTP --> Exits the client \n \n Please enter a number or 0 to exit... \n";
+
+cin >> input;
+switch (input) {
+
+case 0:
+	exitloop=true;
+break;
+
+case 1:
+	kill (c_pid,SIGABRT);	
+break;
+
+case 2:
+	kill (c_pid,SIGFPE);
+break;
+
+case 3:
+	kill (c_pid,SIGILL);
+break;
+
+case 4:
+	kill (c_pid,SIGINT);
+break;
+
+case 5:
+	kill (c_pid,SIGSEGV);
+break;
+
+case 6:
+	kill (c_pid,SIGTERM);
+break;
+
+case 7:
+	kill (c_pid,SIGTSTP);
+	cout << "Exiting server as there is no use of server now" << endl;
+	exitloop=true;
+break;
+
+default :
+ cout << "Number out of bound" <<endl;
+}
+}
+
+return 0;
 }
