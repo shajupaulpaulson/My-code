ef5d33fa838fb40b929af8f5ff7c86d0bd305c9c
Shajupaul Paulson <shaju.eee@gmail.com>
2019-03-24 19:13:58 +0100

    Wolfsburg blues2

--- C++14/controlandquery.cpp
+++ C++14/controlandquery.cpp
@@ -3,3 +3,67 @@ My test examples for modern C++ to learn the latest changes in C++
 https://en.wikipedia.org/wiki/C%2B%2B11 
 https://en.wikipedia.org/wiki/C%2B%2B14 
 ****************************************************************/
+#include <iostream>
+#include <string>
+
+struct test{
+
+	char a;
+	int b;
+	float c;
+	std::string d;
+
+};
+
+class xyz{
+	public:	
+		xyz(){}
+		int k;
+		float l;
+	private:
+		int m;
+		std::string n;
+		char o;
+
+};
+
+struct test2{
+	char a;
+	int b;
+	float c;
+	std::string d;
+	void print4(){
+		std::cout <<"test"<<std::endl;}
+};
+
+union test3{
+
+	char a;
+	int b;
+	float c;
+};
+
+union alignas(128) test4{
+
+	char a;
+	int b;
+	float c;
+};
+
+
+using namespace std;
+int main(){
+	cout << "Aligment of : \n"\
+	"Char " << alignof(char) << "\n" \
+	"int " << alignof(int) << "\n" \
+	"double " << alignof(float) << "\n" \
+	"Bool " << alignof(bool) << "\n" \
+	"test " << alignof(test) << "\n" \
+	"test2 " << alignof(test2) << "\n" \
+	"test3 " << alignof(test3) << "\n" \
+	"test4 " << alignof(test4) << "\n" \
+	"xyz " << alignof(xyz) << "\n"; 
+
+
+	return 0;
+}
--- C++14/enum.cpp
+++ C++14/enum.cpp
@@ -3,3 +3,51 @@ My test examples for modern C++ to learn the latest changes in C++
 https://en.wikipedia.org/wiki/C%2B%2B11 
 https://en.wikipedia.org/wiki/C%2B%2B14 
 ****************************************************************/
+#include <iostream>
+
+enum class Enum1 : bool{
+	wrong,
+	right
+};
+
+enum class Enum2 : unsigned int{
+	Valintial=0,
+	Val1=20,
+	Val2,
+	Val3,
+	Val4
+};
+
+enum class Enum3 : unsigned long{
+	Val1,
+	Val2,
+	Val3,
+	Val4
+};
+
+
+enum struct Enum4 : unsigned long{
+	Val1=10,
+	Val2,
+	Val3,
+	Val4
+};
+
+enum Enum5 : unsigned long{
+	Val1,
+	Val2,
+	Val3,
+	Val4
+};
+
+
+int main(){
+
+	std::cout << "Enum1 : " << (int)Enum1::wrong << "\n" \
+	"Enum2 : " << (int)Enum2::Val2 << "\n" \
+	"Enum3 : " << (int)Enum3::Val2 << "\n" \
+	"Enum4 : " << (int)Enum4::Val2 << "\n" \
+	"Enum5 : " << (int)Enum5::Val2 << "\n" ;
+	
+	return 0;
+}
