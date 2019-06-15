390bbb0aea378331bf95014cd2833c91a4d59b83
Shajupaul Paulson <shaju.eee@gmail.com>
2019-06-15 21:03:02 +0200

    C++14

--- C++14/tuple_types.cpp
+++ C++14/tuple_types.cpp
@@ -3,3 +3,21 @@ My test examples for modern C++ to learn the latest changes in C++
 https://en.wikipedia.org/wiki/C%2B%2B11 
 https://en.wikipedia.org/wiki/C%2B%2B14 
 ****************************************************************/
+
+#include <tuple>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	tuple <string,string, int> emp1 ("ABC", "XYZ", 72);
+	cout << std::get<int>(emp1) << endl;
+	cout << std::get<0>(emp1) << endl;
+	cout << std::get<1>(emp1) << endl;
+	cout << std::get<2>(emp1) << endl;
+
+	std::get<1>(emp1) = "LASTNAME";
+	cout << std::get<0>(emp1) << endl;
+	cout << std::get<1>(emp1) << endl;
+	cout << std::get<2>(emp1) << endl;
+}
--- C++14/wrapper.cpp
+++ C++14/wrapper.cpp
@@ -3,3 +3,28 @@ My test examples for modern C++ to learn the latest changes in C++
 https://en.wikipedia.org/wiki/C%2B%2B11 
 https://en.wikipedia.org/wiki/C%2B%2B14 
 ****************************************************************/
+
+#include <iostream>
+#include <functional>
+
+using namespace std;
+
+void incby2 ( int &r)
+{ r+=2;}
+
+template<class F, class P> void callinc(F f, P r)
+{ f(r); }
+
+int main(){
+	int i=0;
+	callinc(incby2,i);
+	cout << "i = " << i << endl;
+	callinc(incby2,ref(i));
+	cout << "i = " << i << endl;
+	callinc(incby2,i);
+	cout << "i = " << i << endl;
+	callinc(incby2,ref(i));
+	cout << "i = " << i << endl;
+
+	return 0;
+}
