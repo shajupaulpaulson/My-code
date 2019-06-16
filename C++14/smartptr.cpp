f3d821f66a67237fcf48a15307bbc8da9de7a1f1
Shajupaul Paulson <shaju.eee@gmail.com>
2019-06-16 11:01:10 +0200

    restructuring

--- C++14/smartptr.cpp
+++
@@ -1,5 +0,0 @@
-/****************************************************************** 
-My test examples for modern C++ to learn the latest changes in C++ 
-https://en.wikipedia.org/wiki/C%2B%2B11 
-https://en.wikipedia.org/wiki/C%2B%2B14 
-****************************************************************/
